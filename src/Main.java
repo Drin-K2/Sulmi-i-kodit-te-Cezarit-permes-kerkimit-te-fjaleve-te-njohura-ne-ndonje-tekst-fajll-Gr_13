fb54a10826e0c13190ae8d8312f1a81b7f8fb4fb
DuaZogu <dua.zogu@student.uni-pr.edu>
2025-04-12 17:39:09 +0200

    Update Main.java
    Metoda per dekriptim

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,21 @@
-class CaesarCipherAttack {}
+class CaesarCipherAttack {
+  private static final int ALPHABET_SIZE = 26;
+    
+  public static String decrypt(String text, int shift) {
+        StringBuilder result = new StringBuilder();
+       for (char ch : text.toCharArray()) {
+           if (Character.isLetter(ch)) {
+                char base;
+                if (Character.isUpperCase(ch)) {
+                    base = 'A';
+                } else {
+                    base = 'a';
+                }
+                result.append((char) ((ch - base - shift + ALPHABET_SIZE) % ALPHABET_SIZE + base));
+            }  else {
+                result.append(ch);
+            }
+        }
+        return result.toString();
+    }
+}
